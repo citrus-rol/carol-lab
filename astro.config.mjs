92d72acbe13e9fdb3010e6f86b4cc8e2bd04fe7a
citrus-rol <c.citrus.rol@gmail.com>
2025-08-22 01:33:17 +0800

    Prepare for Vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  base: '/carol-lab/',  // 根路徑，對應 GitHub Pages 子路徑
-  outDir: 'docs',           // 輸出資料夾
+  base: '/',
+  outDir: 'docs',
 });
